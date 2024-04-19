9181de9f73e72d8a9445f4a2eb0ffc492fff4a97
asimkoul <ahmadasim080gmail.com>
2024-04-19 13:35:22 +0530

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+const http=require('http')
+const express=require('express')
+const app=express()
+
+const bodyParser=require('body-parser')
+const loginRoute=require('./routes/login')
+const messageRoute=require('./routes/message')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(loginRoute)
+app.use(messageRoute)
+
+app.listen(3000);
